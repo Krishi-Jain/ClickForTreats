668a819535d68aec23a357778879524f836412c1
Krishi Jain <krishijain@fb.com>
2022-07-06 16:38:54 -0700

    Initial Commit

---
+++ ClickForTreats/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ClickForTreats
+//
+//  Created by Krishi Jain on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
